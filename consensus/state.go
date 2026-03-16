c249f09a5e88202471f8845eb0d7dfb48f07084e
prajwal-annigeri <prajwal.annigeri@gmail.com>
2026-03-15 20:15:11 -0400

    feat: phase-wise latencies

--- consensus/state.go
+++ consensus/state.go
@@ -1429,6 +1429,10 @@ func (cs *State) enterPrevote(height int64, round int32) {
 
 	logger.Debug("entering prevote step", "current", log.NewLazySprintf("%v/%v/%v", cs.Height, cs.Round, cs.Step))
 
+	if round == 0 {
+		cs.epochTracker.RecordPrevoteStart(height)
+	}
+
 	// Sign and broadcast vote as necessary
 	cs.doPrevote(height, round)
 
@@ -1545,6 +1549,10 @@ func (cs *State) enterPrecommit(height int64, round int32) {
 	}
 
 	logger.Debug("entering precommit step", "current", log.NewLazySprintf("%v/%v/%v", cs.Height, cs.Round, cs.Step))
+
+	if round == 0 {
+		cs.epochTracker.RecordPrecommitStart(height)
+	}
 
 	defer func() {
 		// Done enterPrecommit:
