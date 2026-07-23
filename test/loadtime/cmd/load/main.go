SeeMoReLab/tendermint

Update load to include monitor

test/loadtime/cmd/load/main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"context"
+	"flag"
 	"fmt"
+	"os"
+	"strings"
+	"time"
 
 	"github.com/google/uuid"
 	"github.com/informalsystems/tm-load-test/pkg/loadtest"
+	"github.com/sirupsen/logrus"
 
 	"github.com/tendermint/tendermint/test/loadtime/payload"
 )
@@ -31,16 +37,67 @@ type TxGenerator struct {
 }
 
 func main() {
+	logrus.SetLevel(logrus.WarnLevel)
+
 	u := [16]byte(uuid.New()) // generate run ID on startup
 	if err := loadtest.RegisterClientFactory("loadtime-client", &ClientFactory{ID: u[:]}); err != nil {
 		panic(err)
 	}
-	loadtest.Run(&loadtest.CLIConfig{
-		AppName:              "loadtime",
-		AppShortDesc:         "Generate timestamped transaction load.",
-		AppLongDesc:          "loadtime generates transaction load for the purpose of measuring the end-to-end latency of a transaction from submission to execution in a Tendermint network.", //nolint:lll
-		DefaultClientFactory: "loadtime-client",
-	})
+
+	if len(os.Args) > 1 {
+		switch os.Args[1] {
+		case "coordinator", "worker", "version", "completion":
+			loadtest.Run(&loadtest.CLIConfig{
+				AppName:              "loadtime",
+				AppShortDesc:         "Generate timestamped transaction load.",
+				AppLongDesc:          "loadtime generates transaction load for the purpose of measuring the end-to-end latency of a transaction from submission to execution in a Tendermint network.",
+				DefaultClientFactory: "loadtime-client",
+			})
+			return
+		}
+	}
+
+	cfg, progressInterval, progressEnabled, verbose, err := parseFlags()
+	if err != nil {
+		if err == flag.ErrHelp {
+			os.Exit(0)
+		}
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(2)
+	}
+
+	if verbose {
+		logrus.SetLevel(logrus.DebugLevel)
+	}
+
+	if err := cfg.Validate(); err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(2)
+	}
+
+	var monitorCancel context.CancelFunc
+	monitorDone := make(chan struct{})
+	if progressEnabled {
+		var monitorCtx context.Context
+		monitorCtx, monitorCancel = context.WithCancel(context.Background())
+		go func() {
+			defer close(monitorDone)
+			runProgressMonitor(monitorCtx, cfg.Endpoints[0], u[:], progressInterval)
+		}()
+	} else {
+		close(monitorDone)
+	}
+
+	err = loadtest.ExecuteStandalone(cfg)
+
+	if monitorCancel != nil {
+		monitorCancel()
+	}
+	<-monitorDone
+
+	if err != nil {
+		os.Exit(1)
+	}
 }
 
 func (f *ClientFactory) ValidateConfig(cfg loadtest.Config) error {
@@ -70,4 +127,77 @@ func (c *TxGenerator) GenerateTx() ([]byte, error) {
 		Size:        c.size,
 		Id:          c.id,
 	})
+}
+
+func parseFlags() (loadtest.Config, time.Duration, bool, bool, error) {
+	var cfg loadtest.Config
+	var endpointsCSV string
+	var progressEvery string
+	var noProgress bool
+	var verbose bool
+
+	fs := flag.NewFlagSet("loadtime", flag.ContinueOnError)
+	fs.SetOutput(os.Stdout)
+
+	fs.StringVar(&cfg.ClientFactory, "client-factory", "loadtime-client", "The identifier of the client factory to use for generating load testing transactions")
+	fs.IntVar(&cfg.Connections, "connections", 1, "The number of connections to open to each endpoint simultaneously")
+	fs.IntVar(&cfg.Connections, "c", 1, "The number of connections to open to each endpoint simultaneously")
+	fs.IntVar(&cfg.Time, "time", 60, "The duration (in seconds) for which to handle the load test")
+	fs.IntVar(&cfg.Time, "T", 60, "The duration (in seconds) for which to handle the load test")
+	fs.IntVar(&cfg.SendPeriod, "send-period", 1, "The period (in seconds) at which to send batches of transactions")
+	fs.IntVar(&cfg.SendPeriod, "p", 1, "The period (in seconds) at which to send batches of transactions")
+	fs.IntVar(&cfg.Rate, "rate", 1000, "The number of transactions to generate each second on each connection, to each endpoint")
+	fs.IntVar(&cfg.Rate, "r", 1000, "The number of transactions to generate each second on each connection, to each endpoint")
+	fs.IntVar(&cfg.Size, "size", 250, "The size of each transaction, in bytes - must be greater than 40")
+	fs.IntVar(&cfg.Size, "s", 250, "The size of each transaction, in bytes - must be greater than 40")
+	fs.IntVar(&cfg.Count, "count", -1, "The maximum number of transactions to send - set to -1 to turn off this limit")
+	fs.IntVar(&cfg.Count, "N", -1, "The maximum number of transactions to send - set to -1 to turn off this limit")
+	fs.StringVar(&cfg.BroadcastTxMethod, "broadcast-tx-method", "async", "The broadcast_tx method to use when submitting transactions - can be async, sync or commit")
+	fs.StringVar(&endpointsCSV, "endpoints", "", "A comma-separated list of URLs indicating Tendermint WebSockets RPC endpoints to which to connect")
+	fs.StringVar(&cfg.EndpointSelectMethod, "endpoint-select-method", loadtest.SelectSuppliedEndpoints, "The method by which to select endpoints")
+	fs.IntVar(&cfg.ExpectPeers, "expect-peers", 0, "The minimum number of peers to expect when crawling the P2P network from the specified endpoint(s) prior to waiting for workers to connect")
+	fs.IntVar(&cfg.MaxEndpoints, "max-endpoints", 0, "The maximum number of endpoints to use for testing, where 0 means unlimited")
+	fs.IntVar(&cfg.PeerConnectTimeout, "peer-connect-timeout", 600, "The number of seconds to wait for all required peers to connect if expect-peers > 0")
+	fs.IntVar(&cfg.MinConnectivity, "min-peer-connectivity", 0, "The minimum number of peers to which each peer must be connected before starting the load test")
+	fs.StringVar(&cfg.StatsOutputFile, "stats-output", "", "Where to store aggregate statistics (in CSV format) for the load test")
+	fs.BoolVar(&verbose, "verbose", false, "Increase output logging verbosity to DEBUG level")
+	fs.BoolVar(&verbose, "v", false, "Increase output logging verbosity to DEBUG level")
+	fs.StringVar(&progressEvery, "progress-interval", "1s", "How often to emit '-- Monitor' lines (e.g. 1s, 2s)")
+	fs.BoolVar(&noProgress, "no-progress", false, "Disable live progress monitor output")
+
+	fs.Usage = func() {
+		fmt.Fprintln(os.Stdout, "loadtime generates transaction load for measuring end-to-end transaction latency.")
+		fmt.Fprintln(os.Stdout, "")
+		fmt.Fprintln(os.Stdout, "Usage:")
+		fmt.Fprintln(os.Stdout, "  load [flags]")
+		fmt.Fprintln(os.Stdout, "")
+		fmt.Fprintln(os.Stdout, "Flags:")
+		fs.PrintDefaults()
+	}
+
+	if err := fs.Parse(os.Args[1:]); err != nil {
+		return loadtest.Config{}, 0, false, false, err
+	}
+	if fs.NArg() > 0 {
+		return loadtest.Config{}, 0, false, false, fmt.Errorf("unexpected arguments: %s", strings.Join(fs.Args(), " "))
+	}
+
+	parts := strings.Split(endpointsCSV, ",")
+	cfg.Endpoints = make([]string, 0, len(parts))
+	for _, p := range parts {
+		p = strings.TrimSpace(p)
+		if p != "" {
+			cfg.Endpoints = append(cfg.Endpoints, p)
+		}
+	}
+
+	interval, err := time.ParseDuration(progressEvery)
+	if err != nil {
+		return loadtest.Config{}, 0, false, false, fmt.Errorf("invalid --progress-interval %q: %w", progressEvery, err)
+	}
+	if interval <= 0 {
+		return loadtest.Config{}, 0, false, false, fmt.Errorf("--progress-interval must be > 0")
+	}
+
+	return cfg, interval, !noProgress, verbose, nil
 }

test/loadtime/cmd/load/monitor.go
@@ -1,0 +1,203 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"net/url"
+	"sort"
+	"strings"
+	"time"
+
+	rpchttp "github.com/tendermint/tendermint/rpc/client/http"
+	"github.com/tendermint/tendermint/test/loadtime/payload"
+	"github.com/tendermint/tendermint/types"
+)
+
+type progressState struct {
+	initialized bool
+	prevHeight  int64
+	prevBlock   *types.Block
+
+	totalFinished int64
+	totalLatency  time.Duration
+}
+
+func runProgressMonitor(ctx context.Context, wsEndpoint string, runID []byte, interval time.Duration) {
+	rpcAddr, err := endpointToRPCAddr(wsEndpoint)
+	if err != nil {
+		fmt.Printf("-- Monitor error: invalid endpoint %q: %v\n", wsEndpoint, err)
+		return
+	}
+
+	client, err := rpchttp.New(rpcAddr, "/websocket")
+	if err != nil {
+		fmt.Printf("-- Monitor error: failed to create RPC client for %q: %v\n", rpcAddr, err)
+		return
+	}
+
+	ticker := time.NewTicker(interval)
+	defer ticker.Stop()
+
+	start := time.Now()
+	st := &progressState{}
+
+	for {
+		select {
+		case <-ctx.Done():
+			// Best-effort final flush before exit.
+			sampleCount, sampleLatencies := collectFinishedTxs(context.Background(), client, runID, st)
+			printProgressLine(start, st, sampleCount, sampleLatencies)
+			return
+		case <-ticker.C:
+			sampleCount, sampleLatencies := collectFinishedTxs(ctx, client, runID, st)
+			printProgressLine(start, st, sampleCount, sampleLatencies)
+		}
+	}
+}
+
+func collectFinishedTxs(ctx context.Context, client *rpchttp.HTTP, runID []byte, st *progressState) (int64, []time.Duration) {
+	status, err := client.Status(ctx)
+	if err != nil {
+		fmt.Printf("-- Monitor error: status query failed: %v\n", err)
+		return 0, nil
+	}
+
+	latest := status.SyncInfo.LatestBlockHeight
+	if latest <= 0 {
+		return 0, nil
+	}
+
+	if !st.initialized {
+		b, err := client.Block(ctx, &latest)
+		if err != nil {
+			fmt.Printf("-- Monitor error: initial block query failed at height %d: %v\n", latest, err)
+			return 0, nil
+		}
+		st.initialized = true
+		st.prevHeight = latest
+		st.prevBlock = b.Block
+		return 0, nil
+	}
+
+	var sampleCount int64
+	var sampleLatencies []time.Duration
+
+	for st.prevHeight+1 <= latest {
+		curHeight := st.prevHeight + 1
+		cur, err := client.Block(ctx, &curHeight)
+		if err != nil {
+			fmt.Printf("-- Monitor error: block query failed at height %d: %v\n", curHeight, err)
+			break
+		}
+
+		for _, tx := range st.prevBlock.Data.Txs {
+			p, err := payload.FromBytes(tx)
+			if err != nil {
+				continue
+			}
+			if !bytes.Equal(p.Id, runID) {
+				continue
+			}
+			if p.Time == nil {
+				continue
+			}
+
+			latency := cur.Block.Time.Sub(p.Time.AsTime())
+			sampleCount++
+			sampleLatencies = append(sampleLatencies, latency)
+			st.totalFinished++
+			st.totalLatency += latency
+		}
+
+		st.prevBlock = cur.Block
+		st.prevHeight = curHeight
+	}
+
+	return sampleCount, sampleLatencies
+}
+
+func printProgressLine(start time.Time, st *progressState, sampleCount int64, sampleLatencies []time.Duration) {
+	elapsed := int(time.Since(start).Seconds())
+
+	if sampleCount == 0 {
+		fmt.Printf("-- Monitor t=%ds finished_total=%d finished_delta=0 avg_latency_ms=NA p95_latency_ms=NA\n",
+			elapsed, st.totalFinished)
+		return
+	}
+
+	sampleAvg := durationAvg(sampleLatencies)
+	p95 := durationPercentile(sampleLatencies, 95)
+	totalAvg := time.Duration(0)
+	if st.totalFinished > 0 {
+		totalAvg = time.Duration(int64(st.totalLatency) / st.totalFinished)
+	}
+
+	fmt.Printf("-- Monitor t=%ds finished_total=%d finished_delta=%d avg_latency_ms=%.2f p95_latency_ms=%.2f total_avg_latency_ms=%.2f\n",
+		elapsed,
+		st.totalFinished,
+		sampleCount,
+		durationMs(sampleAvg),
+		durationMs(p95),
+		durationMs(totalAvg),
+	)
+}
+
+func durationAvg(v []time.Duration) time.Duration {
+	if len(v) == 0 {
+		return 0
+	}
+	var sum int64
+	for _, d := range v {
+		sum += int64(d)
+	}
+	return time.Duration(sum / int64(len(v)))
+}
+
+func durationPercentile(v []time.Duration, p float64) time.Duration {
+	if len(v) == 0 {
+		return 0
+	}
+	sorted := make([]time.Duration, len(v))
+	copy(sorted, v)
+	sort.Slice(sorted, func(i, j int) bool { return sorted[i] < sorted[j] })
+
+	idx := int((p / 100.0) * float64(len(sorted)))
+	if idx >= len(sorted) {
+		idx = len(sorted) - 1
+	}
+	return sorted[idx]
+}
+
+func durationMs(d time.Duration) float64 {
+	return float64(d) / float64(time.Millisecond)
+}
+
+func endpointToRPCAddr(endpoint string) (string, error) {
+	u, err := url.Parse(endpoint)
+	if err != nil {
+		return "", err
+	}
+	switch u.Scheme {
+	case "ws":
+		u.Scheme = "http"
+	case "wss":
+		u.Scheme = "https"
+	case "http", "https":
+		// ok
+	default:
+		return "", fmt.Errorf("unsupported endpoint scheme %q", u.Scheme)
+	}
+
+	if strings.HasSuffix(u.Path, "/websocket") {
+		u.Path = strings.TrimSuffix(u.Path, "/websocket")
+	}
+	u.RawQuery = ""
+	u.Fragment = ""
+
+	if u.Path == "" {
+		return u.String(), nil
+	}
+	// Keep any explicit non-websocket prefix path, e.g. /rpc.
+	return strings.TrimSuffix(u.String(), "/"), nil
+}

SeeMoReLab/cometbft

test/loadtime/cmd/load/main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"context"
+	"flag"
 	"fmt"
+	"os"
+	"strings"
+	"time"
 
 	"github.com/google/uuid"
 	"github.com/informalsystems/tm-load-test/pkg/loadtest"
+	"github.com/sirupsen/logrus"
 
 	"github.com/cometbft/cometbft/test/loadtime/payload"
 )
@@ -31,16 +37,67 @@ type TxGenerator struct {
 }
 
 func main() {
+	logrus.SetLevel(logrus.WarnLevel)
+
 	u := [16]byte(uuid.New()) // generate run ID on startup
 	if err := loadtest.RegisterClientFactory("loadtime-client", &ClientFactory{ID: u[:]}); err != nil {
 		panic(err)
 	}
-	loadtest.Run(&loadtest.CLIConfig{
-		AppName:              "loadtime",
-		AppShortDesc:         "Generate timestamped transaction load.",
-		AppLongDesc:          "loadtime generates transaction load for the purpose of measuring the end-to-end latency of a transaction from submission to execution in a CometBFT network.",
-		DefaultClientFactory: "loadtime-client",
-	})
+
+	if len(os.Args) > 1 {
+		switch os.Args[1] {
+		case "coordinator", "worker", "version", "completion":
+			loadtest.Run(&loadtest.CLIConfig{
+				AppName:              "loadtime",
+				AppShortDesc:         "Generate timestamped transaction load.",
+				AppLongDesc:          "loadtime generates transaction load for the purpose of measuring the end-to-end latency of a transaction from submission to execution in a CometBFT network.",
+				DefaultClientFactory: "loadtime-client",
+			})
+			return
+		}
+	}
+
+	cfg, progressInterval, progressEnabled, verbose, err := parseFlags()
+	if err != nil {
+		if err == flag.ErrHelp {
+			os.Exit(0)
+		}
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(2)
+	}
+
+	if verbose {
+		logrus.SetLevel(logrus.DebugLevel)
+	}
+
+	if err := cfg.Validate(); err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(2)
+	}
+
+	var monitorCancel context.CancelFunc
+	monitorDone := make(chan struct{})
+	if progressEnabled {
+		var monitorCtx context.Context
+		monitorCtx, monitorCancel = context.WithCancel(context.Background())
+		go func() {
+			defer close(monitorDone)
+			runProgressMonitor(monitorCtx, cfg.Endpoints[0], u[:], progressInterval)
+		}()
+	} else {
+		close(monitorDone)
+	}
+
+	err = loadtest.ExecuteStandalone(cfg)
+
+	if monitorCancel != nil {
+		monitorCancel()
+	}
+	<-monitorDone
+
+	if err != nil {
+		os.Exit(1)
+	}
 }
 
 func (f *ClientFactory) ValidateConfig(cfg loadtest.Config) error {
@@ -70,4 +127,77 @@ func (c *TxGenerator) GenerateTx() ([]byte, error) {
 		Size:        c.size,
 		Id:          c.id,
 	})
+}
+
+func parseFlags() (loadtest.Config, time.Duration, bool, bool, error) {
+	var cfg loadtest.Config
+	var endpointsCSV string
+	var progressEvery string
+	var noProgress bool
+	var verbose bool
+
+	fs := flag.NewFlagSet("loadtime", flag.ContinueOnError)
+	fs.SetOutput(os.Stdout)
+
+	fs.StringVar(&cfg.ClientFactory, "client-factory", "loadtime-client", "The identifier of the client factory to use for generating load testing transactions")
+	fs.IntVar(&cfg.Connections, "connections", 1, "The number of connections to open to each endpoint simultaneously")
+	fs.IntVar(&cfg.Connections, "c", 1, "The number of connections to open to each endpoint simultaneously")
+	fs.IntVar(&cfg.Time, "time", 60, "The duration (in seconds) for which to handle the load test")
+	fs.IntVar(&cfg.Time, "T", 60, "The duration (in seconds) for which to handle the load test")
+	fs.IntVar(&cfg.SendPeriod, "send-period", 1, "The period (in seconds) at which to send batches of transactions")
+	fs.IntVar(&cfg.SendPeriod, "p", 1, "The period (in seconds) at which to send batches of transactions")
+	fs.IntVar(&cfg.Rate, "rate", 1000, "The number of transactions to generate each second on each connection, to each endpoint")
+	fs.IntVar(&cfg.Rate, "r", 1000, "The number of transactions to generate each second on each connection, to each endpoint")
+	fs.IntVar(&cfg.Size, "size", 250, "The size of each transaction, in bytes - must be greater than 40")
+	fs.IntVar(&cfg.Size, "s", 250, "The size of each transaction, in bytes - must be greater than 40")
+	fs.IntVar(&cfg.Count, "count", -1, "The maximum number of transactions to send - set to -1 to turn off this limit")
+	fs.IntVar(&cfg.Count, "N", -1, "The maximum number of transactions to send - set to -1 to turn off this limit")
+	fs.StringVar(&cfg.BroadcastTxMethod, "broadcast-tx-method", "async", "The broadcast_tx method to use when submitting transactions - can be async, sync or commit")
+	fs.StringVar(&endpointsCSV, "endpoints", "", "A comma-separated list of URLs indicating Tendermint WebSockets RPC endpoints to which to connect")
+	fs.StringVar(&cfg.EndpointSelectMethod, "endpoint-select-method", loadtest.SelectSuppliedEndpoints, "The method by which to select endpoints")
+	fs.IntVar(&cfg.ExpectPeers, "expect-peers", 0, "The minimum number of peers to expect when crawling the P2P network from the specified endpoint(s) prior to waiting for workers to connect")
+	fs.IntVar(&cfg.MaxEndpoints, "max-endpoints", 0, "The maximum number of endpoints to use for testing, where 0 means unlimited")
+	fs.IntVar(&cfg.PeerConnectTimeout, "peer-connect-timeout", 600, "The number of seconds to wait for all required peers to connect if expect-peers > 0")
+	fs.IntVar(&cfg.MinConnectivity, "min-peer-connectivity", 0, "The minimum number of peers to which each peer must be connected before starting the load test")
+	fs.StringVar(&cfg.StatsOutputFile, "stats-output", "", "Where to store aggregate statistics (in CSV format) for the load test")
+	fs.BoolVar(&verbose, "verbose", false, "Increase output logging verbosity to DEBUG level")
+	fs.BoolVar(&verbose, "v", false, "Increase output logging verbosity to DEBUG level")
+	fs.StringVar(&progressEvery, "progress-interval", "1s", "How often to emit '-- Monitor' lines (e.g. 1s, 2s)")
+	fs.BoolVar(&noProgress, "no-progress", false, "Disable live progress monitor output")
+
+	fs.Usage = func() {
+		fmt.Fprintln(os.Stdout, "loadtime generates transaction load for measuring end-to-end transaction latency.")
+		fmt.Fprintln(os.Stdout, "")
+		fmt.Fprintln(os.Stdout, "Usage:")
+		fmt.Fprintln(os.Stdout, "  load [flags]")
+		fmt.Fprintln(os.Stdout, "")
+		fmt.Fprintln(os.Stdout, "Flags:")
+		fs.PrintDefaults()
+	}
+
+	if err := fs.Parse(os.Args[1:]); err != nil {
+		return loadtest.Config{}, 0, false, false, err
+	}
+	if fs.NArg() > 0 {
+		return loadtest.Config{}, 0, false, false, fmt.Errorf("unexpected arguments: %s", strings.Join(fs.Args(), " "))
+	}
+
+	parts := strings.Split(endpointsCSV, ",")
+	cfg.Endpoints = make([]string, 0, len(parts))
+	for _, p := range parts {
+		p = strings.TrimSpace(p)
+		if p != "" {
+			cfg.Endpoints = append(cfg.Endpoints, p)
+		}
+	}
+
+	interval, err := time.ParseDuration(progressEvery)
+	if err != nil {
+		return loadtest.Config{}, 0, false, false, fmt.Errorf("invalid --progress-interval %q: %w", progressEvery, err)
+	}
+	if interval <= 0 {
+		return loadtest.Config{}, 0, false, false, fmt.Errorf("--progress-interval must be > 0")
+	}
+
+	return cfg, interval, !noProgress, verbose, nil
 }

test/loadtime/cmd/load/monitor.go
@@ -1,0 +1,203 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"net/url"
+	"sort"
+	"strings"
+	"time"
+
+	rpchttp "github.com/cometbft/cometbft/rpc/client/http"
+	"github.com/cometbft/cometbft/test/loadtime/payload"
+	"github.com/cometbft/cometbft/types"
+)
+
+type progressState struct {
+	initialized bool
+	prevHeight  int64
+	prevBlock   *types.Block
+
+	totalFinished int64
+	totalLatency  time.Duration
+}
+
+func runProgressMonitor(ctx context.Context, wsEndpoint string, runID []byte, interval time.Duration) {
+	rpcAddr, err := endpointToRPCAddr(wsEndpoint)
+	if err != nil {
+		fmt.Printf("-- Monitor error: invalid endpoint %q: %v\n", wsEndpoint, err)
+		return
+	}
+
+	client, err := rpchttp.New(rpcAddr, "/websocket")
+	if err != nil {
+		fmt.Printf("-- Monitor error: failed to create RPC client for %q: %v\n", rpcAddr, err)
+		return
+	}
+
+	ticker := time.NewTicker(interval)
+	defer ticker.Stop()
+
+	start := time.Now()
+	st := &progressState{}
+
+	for {
+		select {
+		case <-ctx.Done():
+			// Best-effort final flush before exit.
+			sampleCount, sampleLatencies := collectFinishedTxs(context.Background(), client, runID, st)
+			printProgressLine(start, st, sampleCount, sampleLatencies)
+			return
+		case <-ticker.C:
+			sampleCount, sampleLatencies := collectFinishedTxs(ctx, client, runID, st)
+			printProgressLine(start, st, sampleCount, sampleLatencies)
+		}
+	}
+}
+
+func collectFinishedTxs(ctx context.Context, client *rpchttp.HTTP, runID []byte, st *progressState) (int64, []time.Duration) {
+	status, err := client.Status(ctx)
+	if err != nil {
+		fmt.Printf("-- Monitor error: status query failed: %v\n", err)
+		return 0, nil
+	}
+
+	latest := status.SyncInfo.LatestBlockHeight
+	if latest <= 0 {
+		return 0, nil
+	}
+
+	if !st.initialized {
+		b, err := client.Block(ctx, &latest)
+		if err != nil {
+			fmt.Printf("-- Monitor error: initial block query failed at height %d: %v\n", latest, err)
+			return 0, nil
+		}
+		st.initialized = true
+		st.prevHeight = latest
+		st.prevBlock = b.Block
+		return 0, nil
+	}
+
+	var sampleCount int64
+	var sampleLatencies []time.Duration
+
+	for st.prevHeight+1 <= latest {
+		curHeight := st.prevHeight + 1
+		cur, err := client.Block(ctx, &curHeight)
+		if err != nil {
+			fmt.Printf("-- Monitor error: block query failed at height %d: %v\n", curHeight, err)
+			break
+		}
+
+		for _, tx := range st.prevBlock.Data.Txs {
+			p, err := payload.FromBytes(tx)
+			if err != nil {
+				continue
+			}
+			if !bytes.Equal(p.Id, runID) {
+				continue
+			}
+			if p.Time == nil {
+				continue
+			}
+
+			latency := cur.Block.Time.Sub(p.Time.AsTime())
+			sampleCount++
+			sampleLatencies = append(sampleLatencies, latency)
+			st.totalFinished++
+			st.totalLatency += latency
+		}
+
+		st.prevBlock = cur.Block
+		st.prevHeight = curHeight
+	}
+
+	return sampleCount, sampleLatencies
+}
+
+func printProgressLine(start time.Time, st *progressState, sampleCount int64, sampleLatencies []time.Duration) {
+	elapsed := int(time.Since(start).Seconds())
+
+	if sampleCount == 0 {
+		fmt.Printf("-- Monitor t=%ds finished_total=%d finished_delta=0 avg_latency_ms=NA p95_latency_ms=NA\n",
+			elapsed, st.totalFinished)
+		return
+	}
+
+	sampleAvg := durationAvg(sampleLatencies)
+	p95 := durationPercentile(sampleLatencies, 95)
+	totalAvg := time.Duration(0)
+	if st.totalFinished > 0 {
+		totalAvg = time.Duration(int64(st.totalLatency) / st.totalFinished)
+	}
+
+	fmt.Printf("-- Monitor t=%ds finished_total=%d finished_delta=%d avg_latency_ms=%.2f p95_latency_ms=%.2f total_avg_latency_ms=%.2f\n",
+		elapsed,
+		st.totalFinished,
+		sampleCount,
+		durationMs(sampleAvg),
+		durationMs(p95),
+		durationMs(totalAvg),
+	)
+}
+
+func durationAvg(v []time.Duration) time.Duration {
+	if len(v) == 0 {
+		return 0
+	}
+	var sum int64
+	for _, d := range v {
+		sum += int64(d)
+	}
+	return time.Duration(sum / int64(len(v)))
+}
+
+func durationPercentile(v []time.Duration, p float64) time.Duration {
+	if len(v) == 0 {
+		return 0
+	}
+	sorted := make([]time.Duration, len(v))
+	copy(sorted, v)
+	sort.Slice(sorted, func(i, j int) bool { return sorted[i] < sorted[j] })
+
+	idx := int((p / 100.0) * float64(len(sorted)))
+	if idx >= len(sorted) {
+		idx = len(sorted) - 1
+	}
+	return sorted[idx]
+}
+
+func durationMs(d time.Duration) float64 {
+	return float64(d) / float64(time.Millisecond)
+}
+
+func endpointToRPCAddr(endpoint string) (string, error) {
+	u, err := url.Parse(endpoint)
+	if err != nil {
+		return "", err
+	}
+	switch u.Scheme {
+	case "ws":
+		u.Scheme = "http"
+	case "wss":
+		u.Scheme = "https"
+	case "http", "https":
+		// ok
+	default:
+		return "", fmt.Errorf("unsupported endpoint scheme %q", u.Scheme)
+	}
+
+	if strings.HasSuffix(u.Path, "/websocket") {
+		u.Path = strings.TrimSuffix(u.Path, "/websocket")
+	}
+	u.RawQuery = ""
+	u.Fragment = ""
+
+	if u.Path == "" {
+		return u.String(), nil
+	}
+	// Keep any explicit non-websocket prefix path, e.g. /rpc.
+	return strings.TrimSuffix(u.String(), "/"), nil
+}